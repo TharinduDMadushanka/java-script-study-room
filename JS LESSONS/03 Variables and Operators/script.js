9ea822caf75a75b18accdf3526eeddb67291e177
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-31 19:10:18 +0530

    first commit

--- JS LESSONS/03 Variables and Operators/script.js
+++ JS LESSONS/03 Variables and Operators/script.js
@@ -5,3 +5,17 @@ console.log(name);
 
 let age=25;
 console.log(age);
+
+age=30;
+console.log(age);
+
+const title="Manager";
+console.log(title);
+
+/**
+ * Const cany re assing
+title="Supervisor";
+console.log(title);
+ */
+
+let fullName="Tharindu Madushanka";
