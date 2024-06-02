5ea77f884feecbef7e8015bf9720d888ae498345
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-02 14:35:44 +0530

    first commit

---
+++ JS LESSONS/04 Functions/script.js
@@ -0,0 +1,21 @@
+let fName="Saman";
+let lName="Kumara";
+
+console.log(fName+" "+lName);
+
+function displayName(){
+
+    let fName="Saman";
+    let lName="Kumara";
+
+    console.log(fName+" "+lName); // nothing happen
+}
+
+displayName();
+
+function displayName(fName,lName){
+    console.log(fName+" "+lName);
+}
+
+displayName("Tharindu","Madushanka");
+
