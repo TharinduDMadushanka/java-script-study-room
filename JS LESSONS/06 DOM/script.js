25409465dcf1d70b85f52a9da6deab48e2043b19
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-02 18:42:02 +0530

    first commit

---
+++ JS LESSONS/06 DOM/script.js
@@ -0,0 +1,6 @@
+// DOM --> Document Object Model
+
+let titleElement= document.getElementById("title");
+console.log(titleElement);
+console.log(titleElement.innerHTML);
+titleElement.innerHTML="Changed";
