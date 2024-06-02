a998abd31c18fb2a55efc1128b4aa1ae8090f07c
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-02 19:00:43 +0530

    first commit

--- JS LESSONS/06 DOM/script.js
+++ JS LESSONS/06 DOM/script.js
@@ -4,3 +4,9 @@ let titleElement= document.getElementById("title");
 console.log(titleElement);
 console.log(titleElement.innerHTML);
 titleElement.innerHTML="Changed";
+
+let paragrph = document.getElementsByClassName("pargraph");
+console.log(paragrph);
+
+let textList =document.getElementsByTagName("p");
+console.log(textList);
