92f53b029e0d1c1d7daeb7090c973815e2ed95b8
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-08-13 14:42:22 +0530

    Number Conversion

--- full-js-course/Scripts/data-types.js
+++ full-js-course/Scripts/data-types.js
@@ -22,3 +22,32 @@ console.log(typeof (c))
 console.log(typeof (d))
 console.log(typeof (e))
 console.log(typeof (f))
+
+// ===================== Type Conversion ===========================
+
+// String Conversion
+
+// b = String (b);
+// console.log(b + " is now " + typeof (b))
+
+// c = String(c);
+// console.log(c + " is now " + typeof (c))
+
+// d = String(d);
+// console.log(d + " is now " + typeof (d))
+
+// e = String(e);
+// console.log(e + " is now " + typeof (e))
+
+// f = String(f);
+// console.log(f + " is now " + typeof (f))
+
+
+// Number Conversion
+
+console.log(Number (a))
+console.log(Number (b))
+console.log(Number (c))
+console.log(Number (d))
+console.log(Number (e))
+console.log(Number (f))
