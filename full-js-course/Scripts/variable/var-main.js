6a8c0ca270346c171731696939cb735939a46328
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-08-13 14:17:49 +0530

    how get multiple objects output

--- full-js-course/Scripts/variable/var-main.js
+++ full-js-course/Scripts/variable/var-main.js
@@ -7,3 +7,29 @@ console.error("This is an error!");
 
 // give warnin
 console.warn("This is a warn!");
+
+let a = {
+    name : 'Tharindu',
+    age : 22,
+    city:'Galle'
+}
+
+let b = {
+    name : 'Madu',
+    age : 22,
+    city:'Galle'
+}
+
+let c = {
+    name : 'Shankar',
+    age : 22,
+    city:'Galle'
+}
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+// console.log(a,b,c);
+
+console.log({a,b,c});
