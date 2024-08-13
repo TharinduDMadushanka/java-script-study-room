fd61718c6c7c90c2b50d96d724eae4a3d15350c0
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-08-13 14:12:25 +0530

    error and warn

--- full-js-course/Scripts/variable/var-main.js
+++ full-js-course/Scripts/variable/var-main.js
@@ -1,3 +1,9 @@
 // let yname = prompt('Enter your name ?');
 let yname= confirm('Are you TDM ?');
 console.log(yname);
+
+// give error
+console.error("This is an error!");
+
+// give warnin
+console.warn("This is a warn!");
