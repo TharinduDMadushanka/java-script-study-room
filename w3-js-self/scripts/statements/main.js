d326b3ac3f715a1743e7a2badc49c616bc759383
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-08-13 17:08:58 +0530

    JavaScript Statements

---
+++ w3-js-self/scripts/statements/main.js
@@ -0,0 +1,17 @@
+let x, y, z;  // Statement 1
+x = 5;        // Statement 2
+y = 6;        // Statement 3
+z = x + y;    // Statement 4
+
+
+document.getElementById("demo").innerHTML =
+"The value of z is " + z + ".";  
+
+document.getElementById("name").innerHTML= "TDM";
+
+// JavaScript Code Blocks
+
+function myFunction() {
+    document.getElementById("demo1").innerHTML = "Hello Dolly!";
+    document.getElementById("demo2").innerHTML = "How are you?";
+  }
